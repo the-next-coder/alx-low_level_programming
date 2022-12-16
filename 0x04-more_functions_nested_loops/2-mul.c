be61481e7e6725469e26b7505c58aca1b0e58eb6
the-next-coder <89924695+the-next-coder@users.noreply.github.com>
2022-12-16 01:44:04 +0100

    Create 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+  * mul - mul(int a, int b)
+  * @x: integer
+  * @y: integer
+  * Return: product
+  */
+
+int mul(int x, int y)
+{
+	int product;
+
+	product = x * y;
+	return (product);
+}
