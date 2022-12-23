68b839ca8e22d9a3c03c39faba53bd67dbd039a8
the-next-coder <89924695+the-next-coder@users.noreply.github.com>
2022-12-23 04:45:09 +0100

    Update 101-print_number.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,9 +1,11 @@
 #include "main.h"
+
 /**
- * print_number - prints an integer.
+ * print_number - print an integer.
  * @n: input integer.
  * Return: no return.
  */
+
 void print_number(int n)
 {
 	unsigned int m, d, count;
