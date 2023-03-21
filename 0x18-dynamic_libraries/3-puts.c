33d3815cecebc4e735ce6e93b54b9d51171c1a79
the-next-coder <89924695+the-next-coder@users.noreply.github.com>
2023-03-21 01:09:11 +0100

    Update 3-puts.c

--- 0x18-dynamic_libraries/3-puts.c
+++ 0x18-dynamic_libraries/3-puts.c
@@ -1,19 +1,14 @@
 #include "main.h"
 
 /**
-  * _puts - print string
-  * @str: string
-  * Return: void
-  */
-
+ * _puts - prints a string
+ * @str: string to be printed
+ */
 void _puts(char *str)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
+	while (*str != '\0')
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str++);
 	}
 	_putchar('\n');
 }
